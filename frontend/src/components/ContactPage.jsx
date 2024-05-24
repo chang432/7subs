c685d5ffe336c1ca4fb6f941c91af7950eeef289
chang432 <andre888chang@gmail.com>
2024-05-23 22:57:23 -0400

    add live maps

--- frontend/src/components/ContactPage.jsx
+++ frontend/src/components/ContactPage.jsx
@@ -1,14 +1,48 @@
 import React from "react"
 
-import Location from '../assets/googlemaps.png'
+import styled from 'styled-components';
+
+const MapsDiv = styled.div`
+    height: '100%';
+    width: '100%';
+    maxWidth: '100%';
+
+    width: 400px;
+    height: 200px;
+    overflow: hidden;
+
+    @media (min-width: 768px) {
+    width: 800px;
+    height: 300px;
+    }
+`;
 
 const ContactPage = ({style}) => {
     return (
         <div className="flex flex-col justify-center items-center h-screen pb-52 md:pb-24 space-y-2" style={style}>
-            <h1 className="w-fit text-lg md:text-2xl">617-232-7070</h1>
+            <h1 className="w-fit text-lg md:text-2xl" style={{color: '#FFCC33'}}>617-232-7070</h1>
             <h1 className="w-fit text-lg md:text-2xl">Call us to place an order for pickup</h1>
-            <h1 className="w-fit text-lg md:text-2xl pt-20">1 Centre Street, Brookline, MA 02446</h1>
-            <img src={Location} className="w-3/4"/>
+            <h1 className="w-fit text-lg md:text-2xl pt-5 md:pt-20">1 Centre Street, Brookline, MA 02446</h1>
+            <div className="pt-4" style={{ overflow: 'hidden', maxWidth: '100%', }}>
+                <MapsDiv id="embed-map-display">
+                    <iframe
+                    style={{ height: '100%', width: '100%', border: 0 }}
+                    frameBorder="0"
+                    src="https://www.google.com/maps/embed/v1/place?q=1+Centre+Street,+Brookline,+MA+02446&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+                    allowFullScreen
+                    ></iframe>
+                </MapsDiv>
+                <a className="googlecoder" rel="nofollow" href="https://kbj9qpmy.com/hrn" id="grab-map-data">
+                </a>
+                <style>{`
+                    #embed-map-display img {
+                        max-width: none !important;
+                        background: none !important;
+                        font-size: inherit;
+                        font-weight: inherit;
+                    }
+                `}</style>
+            </div>
         </div>
     );
 }
